a72d4fa85dd1c289f7e2a884d5821a2e4825f85a
nenebeji <105363883+nenebeji@users.noreply.github.com>
2022-09-16 14:51:26 +0100

    initial commit

--- server.js
+++ server.js
@@ -17,16 +17,15 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 // GET Route for homepage
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
-);
-
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
